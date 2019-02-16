66c8817983195e54bed8b681650abbb3317cfd18
rliang <ricardoliang@gmail.com>
2019-02-16 12:58:03 -0200

    Add comments again

--- index.js
+++ index.js
@@ -2,23 +2,30 @@ module.exports = (gen, hash=v => JSON.stringify(v, (k, v) => typeof v === 'funct
   let current = {}, next = value => {
     let result = gen.next(value);
     let updated = {};
+    // Same behavior as `yield*`: ignore return value.
     if (!result.done) {
       for (let effect of result.value) {
+        // Ignore falsy effects in the array.
         if (effect) {
           let key = hash(effect);
-          updated[key] = current[key] || effect[0]((key => (value, done) => {
+          // Transfer existing effect to `updated` if any, or initialize one
+          updated[key] = current[key] || effect[0]((value, done) => {
+            // In case the callback is somehow invoked after the effect is
+            // cancelled.
             if (key in current) {
+              // Immediately cancel effect if done, might need a restart.
               if (done) {
                 current[key]();
                 delete current[key];
               }
               next(value);
             }
-          })(key), ...effect.slice(1));
+          }, ...effect.slice(1));
           delete current[key];
         }
       }
     }
+    // Cancel remaining effects which were not transferred to `updated`.
     for (let key in current)
       current[key]();
     current = updated;
